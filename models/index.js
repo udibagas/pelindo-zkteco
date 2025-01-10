5274683b24f2742c804a3e22f7ecc820828f3618
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-01-10 11:28:29 +0700

    update

--- models/index.js
+++ models/index.js
@@ -6,15 +6,6 @@ class Model {
     const query = `
       SELECT
         *
-        // t.event_time as time,
-        // p.id_card,
-        // p.name,
-        // p.last_name,
-        // p.gender,
-        // p.department,
-        // p.photo_path,
-        // p.auth_dept_id,
-        // p.initial_name
       FROM acc_transaction t
       JOIN pers_person p ON t.pin = p.pin
       ORDER BY id DESC
