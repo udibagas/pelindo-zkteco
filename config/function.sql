1fe328a8810d574010fa8cbfb240c389d9f3ee8a
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-02-28 08:28:59 +0700

    update

--- config/function.sql
+++ config/function.sql
@@ -1,7 +1,7 @@
-DROP TRIGGER IF EXISTS trigger_notify_api;
-DROP FUNCTION IF EXISTS notify_api;
+-- DROP TRIGGER IF EXISTS trigger_notify_api;
+-- DROP FUNCTION IF EXISTS notify_api;
 
-CREATE FUNCTION notify_api()
+CREATE FUNCTION IF NOT EXISTS notify_api()
 RETURNS TRIGGER AS $$
 BEGIN
     PERFORM pg_notify('api_channel', row_to_json(NEW)::TEXT);
@@ -9,6 +9,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER trigger_notify_api
+CREATE TRIGGER IF NOT EXISTS trigger_notify_api
 AFTER INSERT ON acc_transaction
 FOR EACH ROW EXECUTE FUNCTION notify_api();
