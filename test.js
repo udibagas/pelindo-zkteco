7e54217143c9813fb20459de021aa59b070bf756
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-03-10 09:04:50 +0700

    update

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const moveFile = require("./utils/ftp");
 
-moveFile("./test.js", "/a/b/c/test.js")
+moveFile("./test.js", "test.js")
   .then((r) => console.log(r))
   .catch((e) => console.error(e.message));
