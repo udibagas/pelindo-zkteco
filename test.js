c29e7cb6192ac0a481b70de7d783bc636ec384a8
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-03-10 09:07:48 +0700

    jpg only

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const moveFile = require("./utils/ftp");
 
-moveFile("./a.txt", "a.txt")
+moveFile("./test.jpg", "test.jpg")
   .then((r) => console.log(r))
   .catch((e) => console.error(e.message));
