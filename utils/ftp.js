c03df29eb4051f8660b1fbc910c978f4c7002483
udibagas <udibagas@gmail.com>
2025-11-17 13:23:13 +0700

    update

--- utils/ftp.js
+++ utils/ftp.js
@@ -13,10 +13,9 @@ async function moveFile(localFilePath, remoteFilePath) {
       host: process.env.FTP_HOST,
       user: process.env.FTP_USER,
       password: process.env.FTP_PASS,
-      secure: "implicit",
+      secure: false,
       secureOptions: {
         rejectUnauthorized: false,
-        minVersion: "TLSv1.2",
       },
     });
 
