0551f08c06877f3ee6389af1c156df7ec6f649e4
udibagas <udibagas@gmail.com>
2026-08-01 07:57:52 +0700

    perbaiki tampilan transaction log

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ router.use(require("./logs"));
 router.use(require("./transactions"));
 router.use("/api", require("./api"));
 
+router.get("/test", (req, res) => {
+  res.render("test");
+});
+
 module.exports = router;
