01b4f4fe32d635db8726cf7f8ff957b1e53f276a
udibagas <udibagas@gmail.com>
2026-08-02 12:27:37 +0700

    using moment

--- utils/date.js
+++ utils/date.js
@@ -1,9 +1,7 @@
+const moment = require("moment");
+
 function getCurrenDate() {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = (now.getMonth() + 1).toString().padStart(2, "0");
-  const date = now.getDate().toString().padStart(2, "0");
-  return [year, month, date].join("-");
+  return moment().format("YYYY-MM-DD");
 }
 
 module.exports = { getCurrenDate };
