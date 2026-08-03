4d814f25d8f4d1369e78e6cbbea3bfe26010c0dc
udibagas <udibagas@gmail.com>
2026-08-03 11:43:23 +0700

    balikin quality ke 2

--- utils/snapshot.js
+++ utils/snapshot.js
@@ -13,7 +13,7 @@ async function getSnapshot(ip_address, filepath) {
 
     ffmpeg(`rtsp://${ip_address}:8554/stream`)
       .inputOptions(["-rtsp_transport", "tcp"]) // Force TCP transport
-      .outputOptions(["-vframes", "1", "-vcodec", "mjpeg", "-q:v", "10"]) // 1 frame, mjpeg, quality 2
+      .outputOptions(["-vframes", "1", "-vcodec", "mjpeg", "-q:v", "2"]) // 1 frame, mjpeg, quality 2
       .on("start", (cmd) => logger.info("Taking snapshot..."))
       .on("error", (err) => reject(err))
       .on("end", () => resolve("Snapshot taken"))
