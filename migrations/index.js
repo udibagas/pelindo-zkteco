99910f7ce4617fc9262d101685ba941faa516423
udibagas <udibagas@gmail.com>
2026-08-04 19:36:40 +0700

    rapikan migration

--- migrations/index.js
+++ migrations/index.js
@@ -4,8 +4,28 @@ const { argv } = require("process");
 const config = require("../config/config");
 const file = argv[2];
 
-if (!file) {
-  return console.log("Migration file is required!");
+const fileExists = fs.existsSync(`./migrations/${file}`);
+
+if (!file || !fileExists) {
+  const files = fs
+    .readdirSync("./migrations")
+    .filter((f) => ![".", "index.js"].includes(f));
+
+  if (!file) {
+    console.log("Migration file is required!\n");
+  }
+
+  if (!fileExists) {
+    console.log("Invalid migration file!\n");
+  }
+
+  console.log("Available migrations files:");
+
+  files.forEach((f, i) => {
+    console.log(`${++i}. ${f}`);
+  });
+
+  return;
 }
 
 const pool = new Pool(config);
@@ -18,7 +38,7 @@ try {
     if (err) {
       return console.log("Failed to run migration", err.message);
     }
-    console.log("Migration success");
+    console.log("Migration success!");
   });
 } catch (error) {
   console.log(error.message);
