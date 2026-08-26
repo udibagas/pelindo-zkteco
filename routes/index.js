5991f0c7e1879e51c69b17c95d58f2bb1050688f
udibagas <udibagas@gmail.com>
2026-08-26 10:57:12 +0700

    remove test route

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,4 @@ router.use(require("./logs"));
 router.use(require("./transactions"));
 router.use("/api", require("./api"));
 
-router.get("/test", (req, res) => {
-  res.render("test");
-});
-
 module.exports = router;
